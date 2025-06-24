ddba29c5c2348b2ea1015d41371b9dba474d565b
Niv Asaraf <Nivas@moveo.co.il>
2025-06-24 13:43:50 +0300

    Create MapView and ButtonsMap components

--- pockdex/src/App.tsx
+++ pockdex/src/App.tsx
@@ -4,10 +4,15 @@ import { GlobalStyles } from "./styles/GlobalStyles";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import TopBar from "./components/TopBar/TopBar";
-
+import MapView from "./components/MapView/MapView";
 
 function App() {
-
+  
+    const pokemon = {
+            name:"bulbasur",
+            iconUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+            location : {lat:32.07477235615735, lng:34.792342277259536}
+     }
 
   return (
     <>
@@ -15,6 +20,7 @@ function App() {
    <TopBar/>
    <Routes>
       <Route path="/" element={<HomePage/>} />
+      <Route path="/favorites" element={<MapView pokemon = {pokemon}/>} />
     </Routes>
     </>
   )
---
+++ pockdex/src/components/MapButtons/MapButtons.styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+interface MapButtonProps{
+    $selectedMode: string
+}
+export const MapButton = styled.button<MapButtonProps>`
+background-color: ${({$selectedMode,theme})=>$selectedMode === "TRANSIT"? theme.colors.green : theme.colors.lightGray};
+border-radius: 12px;
+padding: 2px 8px;
+font-size: 20px;
+cursor: pointer;
+`;
